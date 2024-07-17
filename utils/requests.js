e5824cf2fb5eba5141b8823a97ef946a015dc7b2
Sanskar Sharma <96559310+sanky20@users.noreply.github.com>
2024-07-17 20:23:30 +0530

    fix4

--- utils/requests.js
+++ utils/requests.js
@@ -2,7 +2,9 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
 async function fetchProperties({ showFeatured = false } = {}) {
-  
+  try {
+    // handle the case where the apiDomain is null
+
     if (!apiDomain) {
       return [];
     }
@@ -17,25 +19,33 @@ async function fetchProperties({ showFeatured = false } = {}) {
     }
 
     return res.json(); // Corrected this line
-  
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 // Fetch single property
 async function fetchProperty(id) {
-
+  try {
     // handle the case where the apiDomain is null
 
     if (!apiDomain) {
       return null;
     }
 
-    const res = await fetch(`${apiDomain}/properties/${id}`);
+    const res = await fetch(`${apiDomain}/properties/${id}`,
+      {next: { revalidate: 0 }},
+    );
 
     if (!res.ok) {
       throw new Error("Failed to fetch");
     }
 
     return res.json(); // Corrected this line
-  
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 export { fetchProperties, fetchProperty };
