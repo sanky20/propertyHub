18d5d3ebfd5297b2966cd3034c536b1752562047
Sanskar Sharma <96559310+sanky20@users.noreply.github.com>
2024-07-17 20:06:33 +0530

    changedd cache no-store to next revalidate

--- utils/requests.js
+++ utils/requests.js
@@ -11,7 +11,7 @@ async function fetchProperties({ showFeatured = false } = {}) {
 
     const res = await fetch(
       `${apiDomain}/properties${showFeatured ? "/featured" : ""}`,
-      { cache: "no-store" }
+      { next: { revalidate: 0 }
     );
 
     if (!res.ok) {
